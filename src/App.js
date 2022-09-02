91f612025a6d963bbd303563fdb350a8b7ded2f6
AmitDey01 <93072082+AmitDey01@users.noreply.github.com>
2022-09-02 14:09:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,12 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import "./App.css";
 
-
-
-
 function App() {
   const [quote, setQuote] = useState([]);
 
   useEffect(() => {
     let data = async () => {
-      let res = await axios.get("http://api.quotable.io/random");
+      let res = await axios.get("https://api.quotable.io/random");
       console.log(res);
       setQuote(res.data);
     };
@@ -18,21 +15,22 @@ function App() {
   }, []);
 
   let fetchData = async () => {
-    let res = await axios.get("http://api.quotable.io/random");
+    let res = await axios.get("https://api.quotable.io/random");
     console.log(res);
     setQuote(res.data);
   };
 
   return (
     <div className="app">
-        <div className="card">
-          <h2 className="heading">{quote.author}</h2>
-          <h3>{quote.content}</h3><br />
-          <button className="button" onClick={fetchData}>
-            <span>NEW QUOTE</span>
-          </button>
-        </div>
+      <div className="card">
+        <h2 className="heading">{quote.author}</h2>
+        <h3>{quote.content}</h3>
+        <br />
+        <button className="button" onClick={fetchData}>
+          <span>NEW QUOTE</span>
+        </button>
       </div>
+    </div>
   );
 }
 
